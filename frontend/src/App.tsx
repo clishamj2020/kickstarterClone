db1905559371d32c517b73bbc72a06677c273835
Michael Clisham <clishamj@miamioh.edu>
2023-12-29 12:57:41 -0500

    Added routing and rendering components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,48 +4,22 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 // <meta name="viewport" content="initial-scale=1, width=device-width" />
 import NavTabs from './components/navTabs';
-import { Button, Grid } from '@mui/material';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomeComponent from './components/homeComponent';
+import ProjectsComponent from './components/projectsComponent';
+import UsersComponent from './components/usersComponent';
 
 function App() {
-    const gridContainerStyle: React.CSSProperties = {
-        textAlign: 'center',
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: '100%',
-    };
-
-    const gridStyle: React.CSSProperties = {
-        textAlign: 'center',
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: '100%',
-    };
-
     return (
         <div>
             <NavTabs />
-            <Grid
-                container
-                // columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-                style={gridContainerStyle}
-
-                // textAlign="center"
-                // border="thick"
-                // borderColor="black"
-            >
-                <Grid style={gridStyle} item xs={3}>
-                    <Button color="success">hi</Button>
-                </Grid>
-                <Grid style={gridStyle} item xs={3}>
-                    <Button color="success">hi</Button>
-                </Grid>
-                <Grid style={gridStyle} item xs={3}>
-                    <Button color="success">hi</Button>
-                </Grid>
-                <Grid style={gridStyle} item xs={3}>
-                    <Button color="success">hi</Button>
-                </Grid>
-            </Grid>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/home" Component={HomeComponent} />
+                    <Route path="/projects" Component={ProjectsComponent} />
+                    <Route path="/users" Component={UsersComponent} />
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
---
+++ frontend/src/components/homeComponent.tsx
@@ -0,0 +1,12 @@
+import React, { ChangeEvent, useState } from 'react';
+
+export default function HomeComponent() {
+    // const [value, setValue] = useState('one');
+
+    // const handleChange = (event: ChangeEvent<{}>, newValue: string) => {
+    //     setValue(newValue);
+    //     console.log(newValue);
+    // };
+
+    return <div></div>;
+}
